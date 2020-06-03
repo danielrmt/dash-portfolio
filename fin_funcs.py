f712b5953f01f79286286b97106f30324b3ced5b
Daniel Ribeiro <danielrmt@gmail.com>
2020-06-03 16:45:12 -0300

    Calculate weights for the frontier portfolios

--- fin_funcs.py
+++ fin_funcs.py
@@ -3,9 +3,10 @@ import pandas as pd
 
 
 class MeanVariancePortfolio:
-    def __init__(self, expected_returns, covariance_matrix):
+    def __init__(self, expected_returns, covariance_matrix, tickers):
         self.expected_returns = expected_returns
         self.covariance_matrix = covariance_matrix
+        self.tickers = tickers
 
     def portfolio_return(self, weights):
         return self.expected_returns.T @ weights
@@ -22,4 +23,16 @@ class MeanVariancePortfolio:
         D = A * C - B ** 2
         df = pd.DataFrame(np.arange(min, max, step), columns=['mu'])
         df['sigma'] = np.sqrt((A * df['mu']**2 - 2 * B * df['mu'] + C) / D)
+
+        w_min = sig_inv @ ones / A
+        w_tan = sig_inv @ self.expected_returns / B
+        
+        df['lambda'] = (C - df['mu'] * B) / D
+        df['gamma'] = (df['mu'] * A - B) / D
+        i = 0
+        for col in self.tickers:
+            w = df['lambda'] * A * w_min[i] + df['gamma'] * B * w_tan[i]
+            df[col] = w
+            i = i + 1
+
         return df
