c83b41661ab51bfa8eb538f8fbe2d00484cf3d01
Daniel Ribeiro <danielrmt@gmail.com>
2020-06-05 14:59:27 -0300

    Display text labels in weights plot

--- app.py
+++ app.py
@@ -423,12 +423,14 @@ def update_weights_plot(fronteira, assets, plot_click):
                     value_name='seleção', var_name='ticker')
             )
 
+    df = df.melt('ticker')
+    df['text'] = round(df['value'] * 100, 1)
     fig = px.scatter(
-        df.melt('ticker'), x='value', y='ticker', color='variable',
+        df, x='value', y='ticker', color='variable', text='text',
         labels={'value': '', 'ticker': ''}
     )
     fig.update_layout(xaxis_tickformat='%')
-    fig.update_traces(marker={'size': 12, 'opacity':.9})
+    fig.update_traces(marker={'size': 12, 'opacity':.9}, textposition='top center')
     fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='#b0b0b0')
 
     return fig
